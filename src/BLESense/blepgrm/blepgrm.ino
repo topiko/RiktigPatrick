e39ef4954b981f0224c55d0339a7567861e74fe7
topiko <topiko1987@gmail.com>
2021-11-22 19:54:29 +0100

    Servos do not detach! :/

--- src/BLESense/blepgrm/blepgrm.ino
+++ src/BLESense/blepgrm/blepgrm.ino
@@ -3,14 +3,20 @@
 #include <Servo.h>
 
 #define REFRESH_INTERVAL 3003 // KST servos operate at 333Hz --> 1/333 * 1e6
+#define SERVODISABLEPERIOD 3003 // Either 0 orREFRESH_INTERVAL ? 
 
 #define I2C_SLAVE_ADDRESS 11 
 #define NSERVOS 2
 #define SERVOUPDATEPERIOD 20 // in ms
+#define COMMTIMEOUT 1000
 
 float x, y, z;
+float wx, wy, wz;
 int n;
-int servopins[2] = {11, 12};
+uint32_t lastInput = 0;
+int servopins[2] = {D8, D9};
+uint8_t mode = 0; // Operation mode: 0 == FAILSAFE
+bool servosattached = false;
 
 struct ServoCtrlStruct {
   int prevUpdate;
@@ -28,7 +34,7 @@ struct StateStruct {
   float accel[3];             // 4*3 = 12
   float w[3];                 // 12
   uint16_t servopos[NSERVOS]; // 2*2 = 4
-  byte paddind[4];            // 4
+  //byte paddind[4];            // 4
                               //------
                               // 32
 };
@@ -39,6 +45,7 @@ ServoCtrlStruct servos[NSERVOS];
 void setup()
 {
   Wire.begin(I2C_SLAVE_ADDRESS);
+  Wire.setClock(400000L);
   Serial.begin(9600);
   Serial.println("BLESense intitalizing:");
   delay(1000);
@@ -55,22 +62,85 @@ void setup()
   Serial.println(" Hz");
 
   // Init servos:
-  for (int i=0;i<NSERVOS;i++) initServos(i, &servos[i]);
+  initServos();
 }
 
-void initServos(int idx, ServoCtrlStruct *servo){
-  // Initialize the various servo paramaters to some values:
-  servo->prevUpdate = 0;
-  servo->speed = 0;
-  servo->minlim = 1400;
-  servo->maxlim = 1600;
-  servo->idx = idx;
-  servo->pos = 1500;
-  servo->maxSpeed = (servo->maxlim - servo->minlim)/1000 * 1/128; 
-  servo->curservo.attach(servopins[idx]);
+void setServoParams(int selector, short val, ServoCtrlStruct *servo){
+  // Set servo params:
+ 
+  switch(selector) {
+    case 0: servo->minlim = val; break;
+    case 1: servo->maxlim = val; break;
+    case 2: servo->maxSpeed = val; break;
+  }
+
+
+}
+
+void setCtrlVals(short selector, short val1, short val2){
+
+  switch(selector) {
+    case 0: servos[0].speed = val1; 
+            servos[1].speed = val2;
+            break;
+    // TODO: set motor speeds:
+  }
+}
+
+//void 
+
+void attachServos(){
+  // Attach all servos:
+  for (int i=0; i<NSERVOS; i++){
+    servos[i].curservo.attach(servopins[i]);
+  }
+  servosattached = true;
+  Serial.println("Servos attached.");
+}
+void detachServos(){
+  // Detach all servos:
+  for (int i=0; i<NSERVOS; i++){
+    servos[i].curservo.writeMicroseconds(SERVODISABLEPERIOD); 
+    servos[i].curservo.detach();
+  }
+  servosattached = false;
+  Serial.println("Servos detached.");
+}
+
+void setMode(short selectmode){
   
-  // Disable servo:
-  servo->curservo.writeMicroseconds(0);
+  if (mode==selectmode) return;
+
+  switch (selectmode) {
+    // FAILSAFE:
+    case 0: mode = 0;
+
+            if (servosattached) detachServos();
+            Serial.println("Entering failsafe mode.");
+            break;
+    // NORMAL:
+    case 1: mode = 1;
+            if (!servosattached) attachServos();
+            Serial.println("Entering normal mode.");
+            break;
+  }
+}
+
+void initServos(){
+
+  for (int i=0; i<NSERVOS; i++){
+    // Initialize the various servo paramaters to some values:
+    servos[i].prevUpdate = 0;
+    servos[i].speed = 0;
+    servos[i].minlim = 1400;
+    servos[i].maxlim = 1600;
+    servos[i].idx = i;
+    servos[i].pos = 1500;
+    servos[i].maxSpeed = float(servos[i].maxlim - servos[i].minlim)/1000.; 
+    
+    // Disable servo:
+    // servos[i].curservo.writeMicroseconds(1500);
+  }
 }
 
 void runServo(ServoCtrlStruct *servo){
@@ -81,7 +151,7 @@ void runServo(ServoCtrlStruct *servo){
   if (SERVOUPDATEPERIOD < sinceUpdate){
     
     // Compute position based on control integer:
-    float speedScale = float(servo->speed / 128);
+    float speedScale = float(servo->speed) / 32768.;
     float speed = servo->maxSpeed * speedScale; 
     int16_t updatePos = sinceUpdate * speed;
     
@@ -92,54 +162,86 @@ void runServo(ServoCtrlStruct *servo){
     state.servopos[servo->idx] = servo->pos;
     
     // Update the servo pos:
-    servo->curservo.writeMicroseconds(servo->pos); 
+    if (mode != 0) {
+      Serial.println("Servo pos: ");
+      Serial.println(servo->pos);
+      Serial.println(servo->speed);
+      Serial.println(speedScale);
+      Serial.println(speed);
+      Serial.println(updatePos);
+      Serial.println();
+
+      servo->curservo.writeMicroseconds(servo->pos); 
+      //servo->curservo.writeMicroseconds(1500); 
+      
+      // Store the update time: (Risk of overflow in??)
+      servo->prevUpdate = millis();
+    }
     
-    // Store the update time: (Risk of overflow in??)
-    servo->prevUpdate = millis();
   }
 }
 
 void loop(){
   
   // Get acceleration:
-  if (IMU.accelerationAvailable()){
+  while (IMU.accelerationAvailable()){
     IMU.readAcceleration(x, y, z);
     state.accel[0] = x;
     state.accel[1] = y;
     state.accel[2] = z;
-    Serial.println("Main loop acc:");
-    Serial.println(state.accel[0]);
-    Serial.println(state.accel[1]);
-    Serial.println(state.accel[2]);
-    
   }
- 
+
+  // Get gyro:
+  while (IMU.gyroscopeAvailable()){
+    IMU.readGyroscope(wx, wy, wz);
+    state.w[0] = wx;
+    state.w[1] = wy;
+    state.w[2] = wz;
+  }
+
   // Drive the servos: 
-  // TODO: IMU not avail while driveServos is called!?!?
-  for (int i=0;i<NSERVOS;1){runServo(&servos[i]);}
+  for (int i=0;i<NSERVOS;i++){runServo(&servos[i]);}
+
+  //TODO: Drive motors.
+  
+  if ((millis() - lastInput) > COMMTIMEOUT){
+    //Go to failsafe if too long since previous ctrl input.
+    if (mode != 0) setMode(0);
+  }
 }
 
 void requestEvents(){
   // Send state array to i2c:
-  Serial.println("Event requested!");
-  Serial.println(state.accel[0]);
-  Serial.println(state.accel[1]);
-  Serial.println(state.accel[2]);
+  // Serial.println("Event requested!");
   Wire.write((byte*) &state, sizeof(state));
 }
 
 void receiveEvents(int numBytes){
 
-  byte buffer[5];
-  Wire.readBytes(buffer, 5);
 
-  Serial.println("Read 5 bytes?");
   
-  for (int i=0;i<5;i++) Serial.println(buffer[i]);
-  /*
-  while (Wire.available()) { // slave may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
-  }*/
-  Serial.println("");
+  lastInput = millis();
+
+  int bufferlen = 6;
+  byte buffer[bufferlen];
+  Wire.readBytes(buffer, bufferlen);
+
+
+  short selector = buffer[1];
+  short val1 = (buffer[2] << 8) | buffer[3]; // Note: conversion from big-endian to little endian system
+  short val2 = (buffer[4] << 8) | buffer[5]; // Note: conversion from big-endian to little endian system
+
+  if (selector<16){ // SET MODE:
+    setMode(selector);
+    //mode = selector;
+  }
+  else if (16<=selector & selector<64){
+    selector = selector - 16;
+    setCtrlVals(selector, val1, val2);
+  }
+  else if (64<=selector){ // SET PARAMS:
+    selector = selector - 64;
+    setServoParams(selector, val2, &servos[val1]);
+  }
+
 }
