e81e5a40330a7f16634514340002898779085385
topiko <topiko1987@gmail.com>
2021-11-24 20:43:27 +0100

    WORKS - appropriate P for pid and arduino is fine.

--- src/RPi/main.py
+++ src/RPi/main.py
@@ -4,14 +4,17 @@ from communication import read_arduino, make_ctrl, write_arduino
 from servo import ServoDriver
 from math import sin
 
-P = 10; D=0;
+P = 20; D=0;
 
-mintheta = -45
-maxtheta = 25
-minphi = -30
-maxphi = 30
+PERIOD = 3
+COMMPERIOD = 20e-3
+
+mintheta = -.45
+maxtheta = .25
+minphi = -40
+maxphi = 40
 htheta_params = {'name':'head_theta',
-                 'max_speed': 90, #[deg/sec]
+                 'max_speed': 120, #[deg/sec]
                  'minlim': mintheta,
                  'maxlim': maxtheta,
                  'idx':1,
@@ -40,7 +43,6 @@ init_dict_head_theta = headtheta.make_init_list()
 init_dict_head_phi = headphi.make_init_list()
 
 
-PERIOD = 3
 
 def init_servo(I2Cbus, init_dict):
 
@@ -55,6 +57,7 @@ def init_servo(I2Cbus, init_dict):
 with SMBus(1) as I2Cbus:
     I2Cbus.pec=1
     t1 = time.time()
+    prev_write = t1
     n = 0
 
     init_servo(I2Cbus, headphi.make_init_list())
@@ -63,7 +66,7 @@ with SMBus(1) as I2Cbus:
     while True:
 
         target_theta = sin((time.time() - t1)/PERIOD * 6.28) * (maxtheta - mintheta)/2 + (maxtheta + mintheta)/2
-        target_phi = sin((time.time() - t1)/PERIOD * 6.28) * (maxphi - minphi)/2 + (maxphi + minphi)/2
+        target_phi = -sin((time.time() - t1)/PERIOD * 6.28) * (maxphi - minphi)/2 + (maxphi + minphi)/2
 
         ax, ay, az, wx, wy, wz, head_phi_pulse, head_theta_pulse, mode = read_arduino(I2Cbus)
 
@@ -72,8 +75,10 @@ with SMBus(1) as I2Cbus:
 
         ctrl_input = make_ctrl(16, phi_speed, theta_speed)
 
-        if (n%10==0):
+        if (n%1000==0):
             print(ax, ay, az, wx, wy, wz)
+            print('Freq: {:.2f}'.format(n/(time.time() - t1)))
+            print()
             print('Arduino mode:  ', mode)
             print('Time: {:.2f}'.format(time.time() - t1))
             print('THETA:')
@@ -81,7 +86,7 @@ with SMBus(1) as I2Cbus:
             print('Theta angle:         {:5.2f} / {:5.2f} / {:5.2f}'.format(headtheta.get_angle(), headtheta.target_angle, target_theta))
             print('Send speed theta:    {:5d} / {:5.4f}'.format(headtheta.speed_to_int(), headtheta.get_speed()))
             print('PHI:')
-            print('Arduino pulse phi:       {:5d} / {:<5d}'.format(head_phi_pulse, headphi.target_pulse))
+            print('Arduino pulse phi:   {:5d} / {:<5d}'.format(head_phi_pulse, headphi.target_pulse))
             print('Phi angle:           {:5.2f} / {:5.2f} / {:5.2f}'.format(headphi.get_angle(), headphi.target_angle, target_phi))
             print('Send speed phi:      {:5d} / {:5.4f}'.format(headphi.speed_to_int(), headphi.get_speed()))
             print()
@@ -89,8 +94,10 @@ with SMBus(1) as I2Cbus:
 
         if (mode==0): ctrl_input = make_ctrl(1,0,0)
 
-        write_arduino(I2Cbus, ctrl_input)
+        if (time.time() - prev_write) > COMMPERIOD:
+            write_arduino(I2Cbus, ctrl_input)
+            prev_write = time.time()
 
         n += 1
 
-        time.sleep(.02)
+        #time.sleep(.02)
