c9b61027146fd272435ded959a75f72afc0732a5
topiko <topiko1987@gmail.com>
2022-04-23 17:46:36 +0200

    Position based.

--- src/remote/keyboard_remote.py
+++ src/remote/keyboard_remote.py
@@ -4,7 +4,7 @@ import struct
 
 HOST = socket.gethostbyname('topikone.local') #'192.168.0.45'
 PORT = 1024
-INC = 10
+INCS = [0, 1, 5, 10]
 tobytes = lambda val, fmt='f': bytearray(struct.pack(fmt, val))
 
 def depack_KB(bytearray_ : bytearray) -> tuple[float, float]:
@@ -20,6 +20,7 @@ if __name__ == '__main__':
         phi, theta = 0., 0.
         exit = False
         mode = 1
+        i = 0
         try:
             while not exit:
                 ctrl = input('Q - quit, s-stop, [h,j,k,l] to steer: ')
@@ -38,6 +39,10 @@ if __name__ == '__main__':
                     phi, theta = 0, 0
                 elif ctrl == 'm':
                     mode = not mode
+                elif ctrl == 'i':
+                    i += 1
+                    if i==len(INCS): i=0
+                    INC = INCS[i]
 
                 if ctrl != 'm':
                     bytearray_ = tobytes(phi) + tobytes(theta) + tobytes(128, '>H')
