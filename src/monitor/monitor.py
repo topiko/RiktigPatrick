3f70b08b41c2005f685af51ebc34d1da1690fdd1
topiko <topiko1987@gmail.com>
2022-04-25 17:00:12 +0200

    Better analysis.

---
+++ src/monitor/monitor.py
@@ -0,0 +1,51 @@
+import socket
+import logging
+import time
+
+import numpy as np
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+LOG = logging.getLogger('monitor_logger')
+
+#HOST = socket.gethostname()
+HOST = socket.gethostbyname('topikone.local') #'192.168.0.45'
+REPORTPORT = 1026
+
+
+def get_monitor_socket(log):
+    report_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            report_sock.connect((HOST, REPORTPORT))
+            break
+        except ConnectionRefusedError:
+            log.error('Failed to connect monitor. Retrying!')
+            time.sleep(2)
+
+    log.info(f'Report socket bound to "{HOST}:{REPORTPORT}".')
+    return report_sock
+
+
+if __name__ == '__main__':
+    LOG.info('Running at: {}'.format(socket.gethostname()))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, REPORTPORT))
+        s.listen()
+        LOG.info(f'Monitor: listening at "{HOST}:{REPORTPORT}"')
+
+        conn, addr = s.accept()
+        with conn:
+            LOG.info('Accepting connection from {}'.format(socket.gethostbyaddr(addr[0])))
+            while True:
+                bytes_ = conn.recv(1024)
+                if bytes_!=b'':
+                    arr = np.frombuffer(bytes_, 'f')
+                    print(arr)
---
+++ src/monitor/plot_run.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+plt.style.use('pltstyle.mplstyle')
+FIGS = plt.rcParams["figure.figsize"]
+
+if __name__ == '__main__':
+    df = pd.read_hdf('data/rp.hdf5')
+
+    times = (df.rptime.values - df.rptime.min())*1000
+    dts = np.diff(times)
+
+    # Comunication times:
+    # =======================
+    title = f'max/avg/min={dts.max():.1f}/{dts.mean():.1f}/{dts.min():.1f} ms. std = {dts.std():.1f} ms.'
+    fig, ax = plt.subplots()
+    bins = np.logspace(1, np.log10(1000), 100, base=10)
+    ax.hist(dts, bins=bins, color='black', alpha=.6)
+    ax.set_title(title)
+
+    ax.semilogx()
+    ax.semilogy()
+    ax.set_ylabel('Count')
+    ax.set_xlabel('rptime diff [ms]')
+    fig.subplots_adjust(bottom=.15)
+    plt.show()
+    # =======================
+
+
+    # Servos+otors+Orietation
+    # =======================
+    fig, (axphi, axtheta, axmot, axrpy) = plt.subplots(4,1, sharex=True, figsize=(FIGS[0], FIGS[1]*2))
+
+    l_d = {'color':'black', 'lw':1.2}
+    for ax, lab in zip((axphi, axtheta), ('phi', 'theta')):
+        target_col = f'head_{lab}_target_angle'
+        value_col = f'head_{lab}_angle'
+
+        ax.plot(times, df.loc[:, value_col], '-x', **l_d, label='Value')
+        ax.plot(times, df.loc[:, target_col], '-.o', **l_d, label='Target')
+        ax.set_title(f'head_{lab}')
+        ax.set_ylabel('deg')
+        ax.legend()
+
+
+    for lab, ls in zip(('roll', 'pitch', 'yaw'), ('-.', '-', '--')):
+        axrpy.plot(times, df.loc[:, lab].values, ls, **l_d, label=lab)
+
+
+    axrpy.legend()
+    axrpy.set_ylabel('deg')
+    axrpy.set_xlabel('Time [ms]')
+    plt.show()
+    # =======================
