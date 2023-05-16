0ffef0a52887ac6a7140f5d63f8b6b127a6c6cd9
topiko <topiko1987@gmail.com>
2023-05-16 20:52:01 +0200

    Minor formatting + reset.

--- src/filters/mahony.py
+++ src/filters/mahony.py
@@ -5,48 +5,44 @@ from filters.qutils import eul2q, q2eul
 
 LOG = logging.getLogger()
 
-class Mahony():
 
+class Mahony:
     def __init__(self):
-        self._qhat = eul2q(0,0,0)
-        self.bhat = np.zeros(3)
-        self.g = np.array([0,0,-1])
+        self._qhat = None
+        self.bhat = None # set in reset
+        self.g = np.array([0, 0, -1])
         self.kp = 1
-        self.ki = .3
+        self.ki = 0.3
+        self.reset()
 
-    def update(self, acc, omega, dt):
-
-
+    def update(self, acc: np.ndarray, gyro: np.ndarray, dt: float):
         if not np.isclose(np.linalg.norm(acc), 1):
-            LOG.warning('Need to normalize acceleration!')
             acc /= np.linalg.norm(acc)
 
         v = acc
         R = qt.as_rotation_matrix(self.qhat)
-        vhat =  -R.T@self.g
+        vhat = -R.T @ self.g
 
         w_mes = np.cross(v, vhat)
 
-
         # P quaternion:
         p = qt.quaternion(0)
-        p.vec = omega - self.bhat + self.kp*w_mes
-
+        p.vec = gyro - self.bhat + self.kp * w_mes
 
         # Q dot:
-        qhatdot = 1/2. * self._qhat*p
+        qhatdot = 1 / 2.0 * self._qhat * p
 
         # bhat dot:
-        bhatdot = -self.ki*w_mes
+        bhatdot = -self.ki * w_mes
 
         # Update:
-        self._qhat += dt*qhatdot
+        self._qhat += dt * qhatdot
 
-        #assert np.isclose(self.qhat.norm(), 1)
+        # assert np.isclose(self.qhat.norm(), 1)
 
         self._qhat /= self.qhat.norm()
 
-        self.bhat += dt*bhatdot
+        self.bhat += dt * bhatdot
 
     @property
     def qhat(self) -> qt.quaternion:
@@ -54,6 +50,8 @@ class Mahony():
 
     @property
     def eul(self) -> np.ndarray:
-        return q2eul(self.qhat)/np.pi*180
+        return q2eul(self.qhat) / np.pi * 180
 
-
+    def reset(self):
+        self.bhat = np.zeros(3)
+        self._qhat = eul2q(0, 0, 0)
