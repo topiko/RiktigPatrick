8d985c4f374026d6b17aa5735444e7c0b87d7e49
topiko <topiko1987@gmail.com>
2023-11-07 20:11:45 +0100

    Sanity checker for the net outputs.

--- src/sim/PIDPolicy.py
+++ src/sim/PIDPolicy.py
@@ -1,4 +1,37 @@
 import numpy as np
+import torch
+
+from sim.envs.rp_env import MAXV
+from sim.nets import PolicyNetwork
+from sim.sim_config import MODEL_INPUT
+
+
+class NetPolicy:
+    def __init__(self):
+        self._net = PolicyNetwork.from_file()
+        self._lock_head = True
+
+    def sample_action(self, obs: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
+        obs_t = torch.concatenate([torch.Tensor(obs[k]) for k in MODEL_INPUT]).view(
+            1, -1
+        )
+
+        act = self._net(obs_t)[0].detach().numpy().astype(float)
+        act = act[0]
+        if any(act > MAXV):
+            raise ValueError()
+
+        d = {
+            "act/left_wheel": act[0].reshape(1, 1),
+            "act/right_wheel": act[1].reshape(1, 1),
+        }
+        if self._lock_head:
+            return d
+
+        d["act/head_pitch"] = np.array([0])
+        d["act/head_turn"] = np.array([0])
+
+        return d
 
 
 class PIDPolicy:
